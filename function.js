1faa746ff56c92e9af89be6c0a50ffab20546937
Isac Bruce (elev) <isac.bruce@elevmail.stockholm.se>
2021-01-04 01:05:22 +0100

    terminal

--- function.js
+++ function.js
@@ -1,3 +1,102 @@
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<p>'+this.txt+'</p>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = (Math.random() * 100) + 10;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+
+var globalhistory = "";
+
+var globalpath = "~/"
+
+// var iTextPos = 0; // initialise text position
+// var sContents = ''; // initialise contents variable
+// var initial_data = "";
+// var iRow; // initialise current row
+
+
+// var addTextToResults = function(textToAdd){
+//
+// this.typewriter = function() {
+//
+//   var u = "<p>" + textToAdd + "</p>";
+//
+//
+//   var aText = new Array(u);
+//
+//   var typeSpeed = 60; // time delay of print out
+//   var iIndex = 0; // start printing array at this posision
+//   var iArrLength = aText[0].length; // the length of the text array
+//   var iScrollAt = 10; // start scrolling up at this many lines
+//   // var iTextPos = 0; // initialise text position
+//   // var sContents = ''; // initialise contents variable
+//   // var iRow; // initialise current row
+//
+//
+//   sContents =  ' ';
+//   iRow = Math.max(0, iIndex-iScrollAt);
+//   var destination = document.getElementById('terminalReslutsCont');
+//
+//   // while ( iRow < iIndex ) {
+//   //   sContents += aText[iRow++] + '<br />';
+//   // }
+//   destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
+//   if ( iTextPos++ == iArrLength ) {
+//     iTextPos = 0;
+//     iIndex++;
+//
+//     if ( iIndex != aText.length ) {
+//       iArrLength = aText[iIndex].length;
+//       setTimeout("typewriter()", typeSpeed);
+//     }
+//   } else {
+//     setTimeout("typewriter()", typeSpeed - Math.random() ** 2 * typeSpeed);
+//   }
+//
+//   // document.getElementById('terminalReslutsCont').innerHTML += "<p>" + this.textToAdd + "</p>";
+// }
+//
+// typewriter(textToAdd);
+//
+// // typewriter();
+// };
+
+
 async function loadText(url) {
     text = await fetch(url);
     //awaits for text.text() prop
@@ -53,10 +152,61 @@ document.addEventListener('DOMContentLoaded', function() {
   // Scroll to the bottom of the results
   scrollToBottomOfResults();
 
-  // Add text to the results div
+  // // Add text to the results div
+  // var addTextToResults = function(textToAdd){
+  //
+  //   document.getElementById('terminalReslutsCont').innerHTML += "<p>" + textToAdd + "</p>";
+  //
+  //   // lol(textToAdd);
+  //
+  //   scrollToBottomOfResults();
+  // }
+
   var addTextToResults = function(textToAdd){
-    document.getElementById('terminalReslutsCont').innerHTML += "<p>" + textToAdd + "</p>";
-    scrollToBottomOfResults();
+
+    var localTextPos = 0; // initialise text position
+
+    var typeSpeed = 60; // time delay of print out
+
+    var textData = "";
+
+
+    if (textToAdd.substring(0,2) != "<p") {
+      textData = "<p>" + textToAdd + "</p>";
+    }
+
+    else {
+      textData = textToAdd;
+    }
+
+
+    this.typewriter = function() {
+
+      var destination = document.getElementById('terminalReslutsCont');
+
+      if (localTextPos == 0) {
+        globalhistory = destination.innerHTML;
+      }
+
+
+
+      globalhistory += textData.substring(localTextPos-1,localTextPos); //getting one element at the position and appending it to the entirety
+
+      destination.innerHTML = globalhistory;
+
+      scrollToBottomOfResults();
+
+      localTextPos++
+
+      // I'm guessing the function overflows and dies but that is no issue
+
+      setTimeout("typewriter()", Math.random() * typeSpeed);
+
+    }
+
+    clearInput();
+
+    typewriter(textToAdd);
   }
 
   // Getting the list of keywords for help & posting it to the screen
@@ -70,7 +220,7 @@ document.addEventListener('DOMContentLoaded', function() {
       "- 'Time' will display the current time.",
       "- 'Date' will display the current date.",
       "- 'tech' will make you expert by watching videos",
-      "* There are more keywords that you have to discover by yourself."
+      "PS. There are more keywords for you to discover. ;)"
     ].join('<br>');
     addTextToResults(helpKeyWords);
   }
@@ -117,75 +267,87 @@ document.addEventListener('DOMContentLoaded', function() {
 
       case "about":
       case "info":
-        clearInput();
+        // clearInput();
         addTextToResults("Hello!<br><br>I am an AI enthusiast with a peculiar inclination for learning and problem-solving. <br>Whenever I am not working on my computer, you will find me running or riding Mountainbike in the local forests.");
         break;
 
       case "details":
-        clearInput();
+        // clearInput();
         addTextToResults("Here are my details:<br><br>Full name: Isac Per Ragnar Bruce<br>Occupation: Student, Developer & Hobby Scientist<br>");
         break;
 
       case "name":
-        clearInput();
+        // clearInput();
         addTextToResults("hello my name is "+dict["name"] + " <p><i>hint: try 'fullname'</i></p>");
         break;
 
+      case "cls":
+      case "clear":
+        // globalhistory = "";
+        document.getElementById('terminalReslutsCont').innerHTML = ""
+        // clearInput();
+        addTextToResults("");
+        break;
+
+      case "pwd":
+        addTextToResults(globalpath)
+        break;
+
       case "fullname":
-        clearInput();
+        // clearInput();
         addTextToResults("My full name is " + dict["fullname"]);
         break;
 
       case "number":
       case "phone":
-        clearInput();
+        // clearInput();
         addTextToResults("You can reach me on mobile too: "+dict["phone"]);
         break;
 
       case "email":
       case "mail":
-        clearInput();
+        // clearInput();
         addTextToResults("You can easely reach me on my email: " + dict["mail"]);
         break;
 
       case "contact":
-        clearInput();
+        // clearInput();
         addTextToResults(dict["contact"] + "<br><br>Phone: "+dict["phone"]+"<br><br>Email: "+dict["mail"]);
         break;
 
       case "location":
       case "address":
-        clearInput();
+        // clearInput();
         addTextToResults("You can find me at: "+dict["location"]);
         break;
 
 
       case "code":
-        clearInput();
+        // clearInput();
         addTextToResults("Source code is available <a href='https://github.com/irreq/irreq.github.io'><u>here</u></a>");
         break;
 
       case "founder":
-        clearInput();
+        // clearInput();
         addTextToResults("Webdevtrick's founder is Shaan");
         break;
 
       case "shaan":
-        clearInput();
+        // clearInput();
         addTextToResults("Shaan is founder of this blog and he is a Developer, SEO, and Graphic Designer");
         break;
 
       case "web development":
       case "web dev":
       case "webdevelopment":
-        clearInput();
+        // clearInput();
         addTextToResults('Web development examples!');
         openLinkInNewWindow('https://webdevtrick.com/web-development/');
         break;
 
       case "hello":
       case "hi":
-        clearInput();
+        // clearInput();
         addTextToResults("Hello, I am your assistant... I am based on pure vanilla JavaScript.");
         break;
 
@@ -194,40 +356,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
       // replies
 
+      case "cd":
+        addTextToResults("Type cd + a path, eg: 'home'.");
+        break;
+
       case "youtube":
-        clearInput();
+        // clearInput();
         addTextToResults("Type youtube + something to search for.");
         break;
 
       case "google":
-        clearInput();
+        // clearInput();
         addTextToResults("Type google + something to search for.");
         break;
 
         case "wiki":
         case "wikipedia":
-          clearInput();
+          // clearInput();
           addTextToResults("Type wiki + something to search for.");
           break;
 
       case "time":
-        clearInput();
+        // clearInput();
         getTimeAndDate("time");
         break;
 
       case "date":
-        clearInput();
+        // clearInput();
         getTimeAndDate("date");
         break;
 
       case "help":
       case "?":
-        clearInput();
+        // clearInput();
         postHelpList();
         break;
 
       default:
-      clearInput();
+      // clearInput();
       addTextToResults("<p><i>The command " + "<b>" + textInputValue + "</b>" + " was not found. Type <b>Help</b> to see all commands.</i></p>");
       break;
     }
@@ -239,10 +405,46 @@ document.addEventListener('DOMContentLoaded', function() {
     textInputValueLowerCase = textInputValue.toLowerCase(); //get the lower case of the string
 
     if (textInputValue != ""){ //checking if text was entered
-      addTextToResults("<p class='userEnteredText'>> " + textInputValue + "</p>");
+      document.getElementById('terminalReslutsCont').innerHTML += "<p class='userEnteredText'>"+globalpath+"> " + textInputValue + "</p>";
+      // addTextToResults("<p class='userEnteredText'>> " + textInputValue + "</p>");
       if (textInputValueLowerCase.substr(0,5) == "open ") { //if the first 5 characters = open + space
         openLinkInNewWindow('http://' + textInputValueLowerCase.substr(5));
         addTextToResults("<i>The URL " + "<b>" + textInputValue.substr(5) + "</b>" + " should be opened now.</i>");
+      } else if (textInputValueLowerCase.substr(0,3) == "cd ") {
+
+        var data = textInputValueLowerCase.substr(3) + " ";
+
+        if (data.substr(0,2) == "/ ") {globalpath = "~/root"}
+        else if (data.substr(0,2) == ". ") {globalpath = "~/"}
+        else if (data.substr(0,3) == ".. "){
+          // go up one place
+
+          if (globalpath.length > "~/".length) {
+            globalpath = globalpath.substr(0,globalpath.lastIndexOf("/"));
+
+          }
+
+          if (globalpath.length < "~/".length) {
+            globalpath = "~/";
+          }
+
+        }
+        else if (data.substr(0,1) == "/" ) {
+
+          globalpath = "~/" + data.substr(1,data.length-1);
+
+        }
+
+        else {
+          if (globalpath.substr(0,globalpath.length) != "~/") {
+            globalpath += "/"
+
+          }
+          globalpath += data.substr(0,data.length-1)
+        }
+
+        addTextToResults("<p><i>hint: try 'ls' to list files</i></p>");
+
       } else if (textInputValueLowerCase.substr(0,8) == "youtube ") {
         openLinkInNewWindow('https://www.youtube.com/results?search_query=' + textInputValueLowerCase.substr(8));
         addTextToResults("<i>I've searched on YouTube for " + "<b>" + textInputValue.substr(8) + "</b>" + " it should be opened now.</i>");
--- old/dump/dump.js
+++ old/dump/dump.js
@@ -43,3 +43,44 @@ var postHelpList = function (){
 
 
 }
+
+
+
+this.typewriter = function() {
+
+  var u = "<p>" + textToAdd + "</p>";
+
+
+  var aText = new Array(u);
+
+  var iSpeed = 60; // time delay of print out
+  var iIndex = 0; // start printing array at this posision
+  var iArrLength = aText[0].length; // the length of the text array
+  var iScrollAt = 20; // start scrolling up at this many lines
+  // var iTextPos = 0; // initialise text position
+  // var sContents = ''; // initialise contents variable
+  // var iRow; // initialise current row
+
+
+  sContents =  ' ';
+  iRow = Math.max(0, iIndex-iScrollAt);
+  var destination = document.getElementById('terminalReslutsCont');
+
+  // while ( iRow < iIndex ) {
+  //   sContents += aText[iRow++] + '<br />';
+  // }
+  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "|";
+  if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+
+    if ( iIndex != aText.length ) {
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", iSpeed);
+    }
+  } else {
+    setTimeout("typewriter()", iSpeed - Math.random() ** 2 * iSpeed);
+  }
+
+  // document.getElementById('terminalReslutsCont').innerHTML += "<p>" + this.textToAdd + "</p>";
+}
